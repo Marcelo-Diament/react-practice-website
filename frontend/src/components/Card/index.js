81e81e24d97cd654f3bec969d4acf939fa40c04a
Marcelo-Diament <marcelodiament@gmail.com>
2021-03-06 13:45:54 -0300

    feature: Card style and tags classNames

--- frontend/src/components/Card/index.js
+++ frontend/src/components/Card/index.js
@@ -3,7 +3,8 @@ import './style.css'
 const Card = () => {
   return (
     <article className="card">
-      <h1>CARD</h1>
+      <h1 className="card__title">CARD</h1>
+      <p className="card__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque expedita, voluptatum distinctio eum inventore eius iusto harum veritatis illum aspernatur nihil libero tempora sequi fuga, officia quae dignissimos quod nostrum?</p>
     </article>
   )
 }
