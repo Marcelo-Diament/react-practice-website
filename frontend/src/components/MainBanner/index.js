31381ea6c416c90460e4c3d79005821f12bfc08d
Marcelo-Diament <marcelodiament@gmail.com>
2021-03-06 20:11:28 -0300

    feature: set bg-img to MainBanner

--- frontend/src/components/MainBanner/index.js
+++ frontend/src/components/MainBanner/index.js
@@ -1,11 +1,23 @@
+import React, { useState, useEffect } from 'react'
+import api from '../../services/api'
 import './style.css'
 
 const MainBanner = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [img, setImg] = useState({})
+  const { getRandomImgByTerm } = api.Unsplash
+
+  const imgCallback = url => {
+    setImg(url)
+    setIsLoading(false)
+  }
+  useEffect(() => getRandomImgByTerm('nature', imgCallback), [])
+
   return (
-    <section className="main-banner">
+    <section className="main-banner" style={{ backgroundImage: isLoading ? '' : `url(${img}` }}>
       <h1 className="main-banner__title">MAIN BANNER</h1>
       <a href="/produto" className="main-banner__btn">Ver mais</a>
-    </section>
+    </section >
   )
 }
 
