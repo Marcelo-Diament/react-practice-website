8144246513d81a34b23dd4ac1acff4eb59f17d8c
Marcelo-Diament <marcelodiament@gmail.com>
2021-03-06 16:14:03 -0300

    feature: Home page

---
+++ frontend/src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import Main from '../../components/Main'
+
+const Home = () => {
+  return (
+    <>
+      <small>Você está na página Home</small>
+      <Main />
+    </>
+  )
+}
+
+export default Home
