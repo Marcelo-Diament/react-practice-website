0ace9e399894b03403309356a66d3b8d32c8ea99
Marcelo-Diament <marcelodiament@gmail.com>
2021-03-06 21:33:36 -0300

    feature: add change img theme feature

--- frontend/src/components/MainBanner/index.js
+++ frontend/src/components/MainBanner/index.js
@@ -4,19 +4,31 @@ import './style.css'
 
 const MainBanner = () => {
   const [isLoading, setIsLoading] = useState(true)
-  const [img, setImg] = useState('')
+  const [img, setImg] = useState('https://i.pinimg.com/originals/65/ba/48/65ba488626025cff82f091336fbf94bb.gif')
+  const [term, setTerm] = useState('nature')
   const { getRandomImgByTerm } = api.Unsplash
 
   const imgCallback = url => {
     setImg(url)
-    setIsLoading(false)
+    setTimeout(
+      setIsLoading(false),
+      1500
+    )
   }
-  useEffect(() => getRandomImgByTerm('nature', imgCallback), [])
+
+  const changeTheme = event => {
+    event.preventDefault()
+    setTerm(prompt('Qual tema deseja?\n\nO termo deve ser escrito em inglês'))
+  }
+
+  useEffect(() => {
+    getRandomImgByTerm(term, imgCallback)
+  }, [getRandomImgByTerm, term])
 
   return (
-    <section className="main-banner" style={{ backgroundImage: isLoading ? '' : `url(${img}` }}>
+    <section className={`main-banner ${isLoading ? 'loading' : ''}`} style={{ backgroundImage: `url(${img}` }}>
       <h1 className="main-banner__title">MAIN BANNER</h1>
-      <a href="/produto" className="main-banner__btn">Ver mais</a>
+      <a href="/produto" className="main-banner__btn" onClick={changeTheme}>Trocar tema</a>
     </section>
   )
 }
