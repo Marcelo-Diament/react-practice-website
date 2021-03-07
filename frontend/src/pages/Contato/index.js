b9bf849ee7510ff4fa79334a1af172e2172944f3
Marcelo-Diament <marcelodiament@gmail.com>
2021-03-07 18:23:19 -0300

    feature: passing Mains children within Contato page

--- frontend/src/pages/Contato/index.js
+++ frontend/src/pages/Contato/index.js
@@ -1,10 +1,15 @@
 import Main from '../../components/Main'
+import MainBanner from '../../components/MainBanner'
+import Form from '../../components/Form'
 
 const Contato = () => {
   return (
     <>
       <small>Você está na página Contato</small>
-      <Main />
+      <Main>
+        <MainBanner/>
+        <Form />
+      </Main>
     </>
   )
 }
