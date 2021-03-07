9bbe5ddd7a793162228abc85f84c1f5fced9fcbb
Marcelo-Diament <marcelodiament@gmail.com>
2021-03-07 19:21:51 -0300

    feature: reading specific content at Card

--- frontend/src/components/Card/index.js
+++ frontend/src/components/Card/index.js
@@ -1,10 +1,11 @@
 import './style.css'
 
-const Card = () => {
+const Card = (props) => {
+  const { title, description } = props
   return (
     <article className="card">
-      <h1 className="card__title">CARD</h1>
-      <p className="card__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque expedita, voluptatum distinctio eum inventore eius iusto harum veritatis illum aspernatur nihil libero tempora sequi fuga, officia quae dignissimos quod nostrum?</p>
+      <h1 className="card__title">{title}</h1>
+      <p className="card__description">{description}</p>
     </article>
   )
 }
