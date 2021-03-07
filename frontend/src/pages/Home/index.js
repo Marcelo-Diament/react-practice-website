0e351b642a6fb3695eed401e8be749bf149b50e8
Marcelo-Diament <marcelodiament@gmail.com>
2021-03-07 18:24:10 -0300

    feature: passing Mains children within Home page

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,10 +1,15 @@
 import Main from '../../components/Main'
+import MainBanner from '../../components/MainBanner'
+import Shelf from '../../components/Shelf'
 
 const Home = () => {
   return (
     <>
       <small>Você está na página Home</small>
-      <Main />
+      <Main>
+        <MainBanner />
+        <Shelf />
+      </Main>
     </>
   )
 }
