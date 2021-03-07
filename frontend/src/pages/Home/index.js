368b38a35306879b79fe74a0e1a7b9558aa69f17
Marcelo-Diament <marcelodiament@gmail.com>
2021-03-07 19:11:29 -0300

    feature: reading content at Home

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,14 +1,23 @@
 import Main from '../../components/Main'
 import MainBanner from '../../components/MainBanner'
 import Shelf from '../../components/Shelf'
+import { content } from '../../Helpers'
 
 const Home = () => {
+  const { categories, posts } = content
+  const renderCategories = () => categories.map((category) => {
+    const { id, title } = category
+    const catPosts = posts.filter(post => post.cat_id === id)
+    return (
+      <Shelf title={title} posts={catPosts} key={id} />
+    )
+  })
   return (
     <>
       <small>Você está na página Home</small>
       <Main>
         <MainBanner title="HOME" />
-        <Shelf />
+        {renderCategories()}
       </Main>
     </>
   )
