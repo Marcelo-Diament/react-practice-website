67c919eb4d2dd68178433a648919cd590a3755af
Marcelo-Diament <marcelodiament@gmail.com>
2021-03-07 19:17:54 -0300

    feature: reading specific content at Shelf

--- frontend/src/components/Shelf/index.js
+++ frontend/src/components/Shelf/index.js
@@ -1,16 +1,20 @@
 import Card from '../Card'
 import './style.css'
 
-const Shelf = () => {
+const Shelf = ({ ...props }) => {
+  const { title, posts } = props
+  const renderPosts = () => {
+    return posts.map((post, index) => {
+      const { title, description } = post
+      return (
+        <Card title={title} description={description} key={index} />
+      )
+    })
+  }
   return (
     <section className="shelf">
-      <h1 className="shelf__title">SHELF</h1>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <h1 className="shelf__title">{title}</h1>
+      {renderPosts()}
     </section>
   )
 }
