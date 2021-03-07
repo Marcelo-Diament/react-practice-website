29869f3bbc71aed11d509093cba5b0aa0726616e
Marcelo-Diament <marcelodiament@gmail.com>
2021-03-07 18:36:40 -0300

    feature: MainBanner receives title

--- frontend/src/components/MainBanner/index.js
+++ frontend/src/components/MainBanner/index.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from 'react'
 import api from '../../services/api'
 import './style.css'
 
-const MainBanner = () => {
+const MainBanner = ({ ...props }) => {
   const { getRandomImgByTerm } = api.Unsplash,
     defaultImg = 'https://i.pinimg.com/originals/65/ba/48/65ba488626025cff82f091336fbf94bb.gif',
     defaultTerm = 'nature',
     [isLoading, setIsLoading] = useState(true),
     [img, setImg] = useState(defaultImg),
-    [term, setTerm] = useState(defaultTerm)
+    [term, setTerm] = useState(defaultTerm),
+    { title } = props
 
   useEffect(() => {
     const imgCallback = url => {
@@ -43,7 +44,7 @@ const MainBanner = () => {
 
   return (
     <section className={`main-banner ${isLoading ? 'loading' : ''}`} style={{ backgroundImage: `url(${img}` }}>
-      <h1 className="main-banner__title">MAIN BANNER</h1>
+      <h1 className="main-banner__title">{title ? title : 'MAIN BANNER'}</h1>
       <a href="/produto" className="main-banner__btn" onClick={changeTheme}>Trocar tema</a>
     </section>
   )
