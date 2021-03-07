ec2444f962168a1557ad52acb3c2e8cf2c6e4bf7
Marcelo-Diament <marcelodiament@gmail.com>
2021-03-06 21:32:59 -0300

    feature: remove some of the fixed terms

--- frontend/src/services/api/Unsplash.js
+++ frontend/src/services/api/Unsplash.js
@@ -1,5 +1,5 @@
 const getRandomImgByTerm = async (term, callback) => {
-  const response = await fetch(`https://source.unsplash.com/1600x600/?${term},dark,landscape,horizontal`)
+  const response = await fetch(`https://source.unsplash.com/1600x600/?${term},horizontal`)
   const url = response.url
   callback(url)
 }
