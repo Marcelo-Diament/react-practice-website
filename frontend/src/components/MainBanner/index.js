b4f6bc5aeca24c2240651e90ea0d8236418b0050
Marcelo-Diament <marcelodiament@gmail.com>
2021-03-06 22:53:20 -0300

    feature: update prompt message

--- frontend/src/components/MainBanner/index.js
+++ frontend/src/components/MainBanner/index.js
@@ -3,10 +3,10 @@ import api from '../../services/api'
 import './style.css'
 
 const MainBanner = () => {
+  const { getRandomImgByTerm } = api.Unsplash
   const [isLoading, setIsLoading] = useState(true)
   const [img, setImg] = useState('https://i.pinimg.com/originals/65/ba/48/65ba488626025cff82f091336fbf94bb.gif')
   const [term, setTerm] = useState('nature')
-  const { getRandomImgByTerm } = api.Unsplash
 
   const imgCallback = url => {
     setImg(url)
@@ -16,14 +16,14 @@ const MainBanner = () => {
     )
   }
 
-  const changeTheme = event => {
-    event.preventDefault()
-    setTerm(prompt('Qual tema deseja?\n\nO termo deve ser escrito em inglês'))
-  }
-
   useEffect(() => {
     getRandomImgByTerm(term, imgCallback)
   }, [getRandomImgByTerm, term])
+
+  const changeTheme = event => {
+    event.preventDefault()
+    setTerm(prompt('Qual tema deseja?\n\nVocê pode definir mais de um tema, separando-os por vírgula. O termo deve ser escrito em inglês.').replace(/ /gi,''))
+  }
 
   return (
     <section className={`main-banner ${isLoading ? 'loading' : ''}`} style={{ backgroundImage: `url(${img}` }}>
