ddf4f388f2d329592eac23c861d8756715efc609
Marcelo-Diament <marcelodiament@gmail.com>
2021-03-07 03:26:43 -0300

    feature: Button component imported to Header

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,6 +1,8 @@
+import { ToggleButton } from '../Button'
 import './style.css'
 
-const Header = () => {
+const Header = ({ ...props }) => {
+  const { themeClick } = props
   return (
     <section className="header">
       <h1 className="header__title">HEADER</h1>
@@ -9,6 +11,7 @@ const Header = () => {
         <a href="/sobre" className="header__nav__item">Sobre</a>
         <a href="/produtos" className="header__nav__item">Produtos</a>
         <a href="/contato" disabled className="header__nav__item--disabled">Contato</a>
+        <ToggleButton className="header__nav__item" themeClick={themeClick} />
       </nav>
     </section>
   )
