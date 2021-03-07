bcd88d9add1c043bb9ed827b18943e5419138647
Marcelo-Diament <marcelodiament@gmail.com>
2021-03-07 19:11:13 -0300

    feature: reading content at Sobre

--- frontend/src/pages/Sobre/index.js
+++ frontend/src/pages/Sobre/index.js
@@ -2,15 +2,24 @@ import Main from '../../components/Main'
 import MainBanner from '../../components/MainBanner'
 import Form from '../../components/Form'
 import Shelf from '../../components/Shelf'
+import { content } from '../../Helpers'
 
 const Sobre = () => {
+  const { categories, posts } = content
+  const renderCategories = () => categories.map((category) => {
+    const { id, title } = category
+    const catPosts = posts.filter(post => post.cat_id === id)
+    return (
+      <Shelf title={title} posts={catPosts} key={id} />
+    )
+  })
   return (
     <>
       <small>Você está na página Sobre</small>
       <Main>
         <MainBanner title="SOBRE" />
         <Form />
-        <Shelf />
+        {renderCategories()}
       </Main>
     </>
   )
