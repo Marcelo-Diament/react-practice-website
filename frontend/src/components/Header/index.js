99ee81b8a24fcca0473192c7f5d9df52517f9b94
Marcelo-Diament <marcelodiament@gmail.com>
2021-03-07 13:12:07 -0300

    documentation: update Header links and style

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -9,8 +9,9 @@ const Header = ({ ...props }) => {
       <nav className="header__nav">
         <a href="/" className="header__nav__item">Início</a>
         <a href="/sobre" className="header__nav__item">Sobre</a>
-        <a href="/produtos" className="header__nav__item">Produtos</a>
-        <a href="/contato" disabled className="header__nav__item--disabled">Contato</a>
+        <a href="/categorias" className="header__nav__item">Categorias</a>
+        <a href="/promocoes" disabled className="header__nav__item--disabled">Promoções</a>
+        <a href="/contato" className="header__nav__item">Contato</a>
         <ToggleButton className="header__nav__item" themeClick={themeClick} />
       </nav>
     </section>
