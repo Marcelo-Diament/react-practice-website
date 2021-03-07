35302be8d81328080be4d06e2b23a679b50d457d
Marcelo-Diament <marcelodiament@gmail.com>
2021-03-07 18:24:56 -0300

    feature: passing Mains children within Sobre page

--- frontend/src/pages/Sobre/index.js
+++ frontend/src/pages/Sobre/index.js
@@ -1,10 +1,17 @@
 import Main from '../../components/Main'
+import MainBanner from '../../components/MainBanner'
+import Form from '../../components/Form'
+import Shelf from '../../components/Shelf'
 
 const Sobre = () => {
   return (
     <>
       <small>Você está na página Sobre</small>
-      <Main />
+      <Main>
+        <MainBanner />
+        <Form />
+        <Shelf />
+      </Main>
     </>
   )
 }
