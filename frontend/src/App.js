22efa7e7c878e00b2f3d08837aaa875fc4507454
Marcelo-Diament <marcelodiament@gmail.com>
2021-03-07 03:40:54 -0300

    feature: App update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,20 @@
+import React, { useState } from 'react'
 import Header from './components/Header'
 import Home from './pages/Home'
 import Footer from './components/Footer'
 import './App.css';
 
 function App() {
+  const lightAsDefault = new Date().getHours() > 8 && new Date().getHours() < 18
+  const [isLight, setIsLight] = useState(lightAsDefault)
+  const toggleTheme = () => setIsLight(!isLight)
   return (
-    <div className="App">
-      <Header />
+    <div className={`App${!isLight ? ' dark' : ''}`}>
+      <Header themeClick={toggleTheme} />
       <Home />
       <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
