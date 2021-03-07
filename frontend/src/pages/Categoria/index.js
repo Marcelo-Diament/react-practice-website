ddbf0126db79db7af837f562b24b9321c3d761ca
Marcelo-Diament <marcelodiament@gmail.com>
2021-03-07 19:10:56 -0300

    feature: reading content at Categoria

--- frontend/src/pages/Categoria/index.js
+++ frontend/src/pages/Categoria/index.js
@@ -1,14 +1,23 @@
 import Main from '../../components/Main'
 import MainBanner from '../../components/MainBanner'
 import Shelf from '../../components/Shelf'
+import { content } from '../../Helpers'
 
 const Categoria = () => {
+  const { categories, posts } = content
+  const renderCategories = () => categories.map((category) => {
+    const { id, title } = category
+    const catPosts = posts.filter(post => post.cat_id === id)
+    return (
+      <Shelf title={title} posts={catPosts} key={id} />
+    )
+  })
   return (
     <>
       <small>Você está na página Categoria</small>
       <Main>
         <MainBanner title="CATEGORIAS" />
-        <Shelf />
+        {renderCategories()}
       </Main>
     </>
   )
